40334d9e97b293f991540f55e9156e16d4797222
Sean Burke <sean.burke@live.ca>
2021-08-21 19:50:47 -0400

    Added a score keeping and message when a player gets to 5 points

--- script.js
+++ script.js
@@ -8,8 +8,14 @@ let myArray = [
 ];
 
 
+let playerScore = 0 
+let computerScore = 0
+
+
 
 function playRound(playerSelection) {
+    
+
     
     playerPlay = prompt("Rock, Paper or Scissors?");
     playerSelection = playerPlay.toLowerCase();
@@ -21,21 +27,39 @@ function playRound(playerSelection) {
     }
    
     if ((playerSelection === "rock" && computerSelection == "Scissors") || 
-    (playerSelection === "paper" &&  computerSelection ===  "Rock") || 
-    (playerSelection === "scissors" &&  computerSelection === "Paper")) {
-        return "You chose " + (playerSelection) + " and the computer chose " + (computerSelection) + " so, You win!"
+        (playerSelection === "paper" &&  computerSelection ===  "Rock") || 
+        (playerSelection === "scissors" &&  computerSelection === "Paper")) {
+        let x = playerScore++
+         return "You chose " + (playerSelection) + " and the computer chose " + (computerSelection) + " so, You win!" +
+         this.scoreKeeping()
+       
     } else if ((playerSelection === "rock" &&  computerSelection === "Rock") || 
-    (playerSelection == "paper" &&  computerSelection ===  "Paper") || 
-    (playerSelection === "scissors" &&  computerSelection ===  "Scissors")) {
-        return "You chose " + (playerSelection) + " and the computer chose " + (computerSelection) + " so, Tie!"
-}   else if ((playerSelection === "rock" &&  computerSelection === "Paper") || 
-(playerSelection == "paper" &&  computerSelection ===  "Scissors") || 
-(playerSelection === "scissors" &&  computerSelection ===  "Rock")) {
-    return "You chose " + (playerSelection) + " and the computer chose " + (computerSelection) + " so, You Lose!"
+        (playerSelection == "paper" &&  computerSelection ===  "Paper") || 
+        (playerSelection === "scissors" &&  computerSelection ===  "Scissors")) {
+        return "You chose " + (playerSelection) + " and the computer chose " + (computerSelection) + " so, Tie!" + this.scoreKeeping()
+    }   else if ((playerSelection === "rock" &&  computerSelection === "Paper") || 
+        (playerSelection == "paper" &&  computerSelection ===  "Scissors") || 
+        (playerSelection === "scissors" &&  computerSelection ===  "Rock")) {
+        let y = computerScore++
+        return "You chose " + (playerSelection) + " and the computer chose " + (computerSelection) + " so, You Lose!" + this.scoreKeeping()
+        
 } else {
     return "Please submit Rock, Paper or Scissors"
+}  
+}  
+
+function scoreKeeping(x, y) {
+   if (playerScore === 5) {
+    return " You win! Well played"
+} else if (computerScore === 5) {
+    return " You lose! Better luck next time"
+}   else {
+    return " Your score is " + (playerScore) + " and the computer has " + (computerScore)
 }
 }
 
+
+        
+    
 
 
